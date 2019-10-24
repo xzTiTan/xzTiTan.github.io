4d9a8ad22ee6f1f99ffa2aa29c3220f99d5cf268
xzTiTan <56799118+xzTiTan@users.noreply.github.com>
2019-10-25 01:28:34 +0300

    Add files via upload

--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,8 @@
-//ПУсто но это пака!
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
